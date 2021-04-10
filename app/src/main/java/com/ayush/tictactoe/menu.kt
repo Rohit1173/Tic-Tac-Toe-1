c3664a49d72a62785aa672ca025bdfde79226f59
ayush0402 <lit2020024@iiitl.ac.in>
2021-04-11 00:04:05 +0530

    first alpha build

--- app/src/main/java/com/ayush/tictactoe/menu.kt
+++ app/src/main/java/com/ayush/tictactoe/menu.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
 
 class menu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
